f32a6f7eb39b679d1a8b065c6a924b92c1c4fc36
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-06-23 20:14:00 +0300

    add books object and first demo book. add create button function

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const header = document.querySelector('.sliding-header');
 const year = document.querySelector('.year');
-
-year.textContent = new Date().getFullYear();
-let isMouseDown = false;
-let currentMode = "paint";
-let gridSize = Number(slider.value);
-let gridVisible = true;
-
+const sideBar = document.querySelector('.sidebar');
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 50){
@@ -15,3 +9,36 @@ window.addEventListener("scroll", () => {
         header.classList.remove('scrolled-header')
     }
 })
+
+
+const books = [
+    {
+        id: 1,
+        name: "Suç ve Ceza",
+        author: "Dostoyevski",
+        pageNumber: 300
+    }
+]
+
+
+function createButtonsToSideBar(){
+    sideBar.innerHTML = "";
+
+    books.forEach((book) => {
+        const newButton = document.createElement("button");
+
+        newButton.textContent = book.name;
+        newButton.id = book.id;
+        newButton.classList.add("side-buttons");
+
+        sideBar.append(newButton);
+    });
+}
+
+createButtonsToSideBar();
+
+year.textContent = new Date().getFullYear();
+let isMouseDown = false;
+let currentMode = "paint";
+let gridSize = Number(slider.value);
+let gridVisible = true;
