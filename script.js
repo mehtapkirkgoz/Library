0e80ae0fcd7b4ef0521e4b43aafc55c8e1c5355e
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-06-24 16:26:32 +0300

    add elements function to main place

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const header = document.querySelector('.sliding-header');
 const year = document.querySelector('.year');
 const sideBar = document.querySelector('.sidebar');
+const section = document.querySelector('.section-place');
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 50){
@@ -16,7 +17,8 @@ const books = [
         id: 1,
         name: "Suç ve Ceza",
         author: "Dostoyevski",
-        pageNumber: 300
+        pageNumber: 300,
+        bookImage: "suc-ve-ceza.jpg"
     }
 ]
 
@@ -31,8 +33,33 @@ function createButtonsToSideBar(){
         newButton.id = book.id;
         newButton.classList.add("side-buttons");
 
+        newButton.addEventListener("click", () => {
+            addBooksIngredientsToMain(book);
+        })
+
         sideBar.append(newButton);
     });
+}
+
+function addBooksIngredientsToMain(book){
+    section.innerHTML = "";
+
+    const bookName = document.createElement("p");
+    bookName.classList.add('book-name-section');
+    bookName.textContent = book.name;
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.classList.add('book-author-section');
+    bookAuthor.textContent = book.author;
+
+    const bookPageNumber = document.createElement("p");
+    bookPageNumber.classList.add('book-page-number-section');
+    bookPageNumber.textContent = `${book.pageNumber} sayfa`;
+
+    const bookImage = document.createElement("img");
+    bookImage.classList.add('book-image-section');
+
+    section.append(bookName, bookAuthor, bookPageNumber, bookImage);
 }
 
 createButtonsToSideBar();
