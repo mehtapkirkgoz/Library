76567922c949cf7b909363073b50980cbc9675a6
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-06-26 16:14:30 +0300

    add show books grid function

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const header = document.querySelector('.sliding-header');
 const year = document.querySelector('.year');
 const sideBar = document.querySelector('.sidebar');
 const section = document.querySelector('.section-place');
+const mainPlace = document.querySelector('.main-box');
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 50){
-        header.classList.add('scrolled-header')
+        header.classList.add('scrolled-header');
     }else{
-        header.classList.remove('scrolled-header')
+        header.classList.remove('scrolled-header');
     }
-})
-
+});
 
 const books = [];
 
@@ -24,18 +24,19 @@ function addBook(id, name, author, pageNumber, bookImage){
     });
 
     createButtonsToSideBar();
+    showBooksGrid();
 }
 
 addBook(1, "Suç ve Ceza", "Dostoyevski", 688, "images/suc-ve-ceza.jpg");
 addBook(2, "Sefiller", "Victor Hugo", 1232, "images/sefiller.jpg");
 addBook(3, "1984", "George Orwell", 328, "images/1984.jpg");
 addBook(4, "Dönüşüm", "Franz Kafka", 104, "images/dönüşüm.jpg");
-addBook(5, "Anna Karenina", "Lev Toystoy", 953, "images/anna-karenina.jpg");
+addBook(5, "Anna Karenina", "Lev Tolstoy", 953, "images/anna-karenina.jpg");
 addBook(6, "Madame Bovary", "Gustave Flaubert", 376, "images/madame-bovary.jpg");
 addBook(7, "Aşk ve Gurur", "Jane Austen", 392, "images/aşk-ve-gurur.jpg");
 addBook(8, "Notre Dame'ın Kamburu", "Victor Hugo", 452, "images/notre-dome-kamburu.jpg");
 addBook(9, "Satranç", "Stefan Zweig", 192, "images/satranc.jpg");
-addBook(9, "Don Kişot", "Miguel de Cervantes", 922, "images/don-kisot.jpg");
+addBook(10, "Don Kişot", "Miguel de Cervantes", 922, "images/don-kisot.jpg");
 
 function createButtonsToSideBar(){
     sideBar.innerHTML = "";
@@ -48,15 +49,18 @@ function createButtonsToSideBar(){
         newButton.classList.add("side-buttons");
 
         newButton.addEventListener("click", () => {
-            addBooksIngredientsToMain(book);
-        })
+            showBookDetail(book);
+        });
 
         sideBar.append(newButton);
     });
 }
 
-function addBooksIngredientsToMain(book){
+function showBookDetail(book){
     section.innerHTML = "";
+
+    section.classList.remove("books-grid");
+    section.classList.add("book-detail");
 
     const bookName = document.createElement("p");
     bookName.classList.add('book-name-section');
@@ -72,16 +76,35 @@ function addBooksIngredientsToMain(book){
 
     const bookImage = document.createElement("img");
     bookImage.classList.add('book-image-section');
-    bookImage.setAttribute('src' , book.bookImage);
+    bookImage.src = book.bookImage;
     bookImage.alt = book.name;
 
     section.append(bookName, bookAuthor, bookPageNumber, bookImage);
 }
 
-createButtonsToSideBar();
+function showBooksGrid(){
+    section.innerHTML = "";
+
+    section.classList.remove("book-detail");
+    section.classList.add("books-grid");
+
+    books.forEach((book) => {
+        const imageButton = document.createElement("button");
+        imageButton.classList.add("book-image-button");
+
+        const image = document.createElement("img");
+        image.classList.add("section-images");
+        image.src = book.bookImage;
+        image.alt = book.name;
+
+        imageButton.append(image);
+
+        imageButton.addEventListener("click", () => {
+            console.log(`${book.name} görseline tıklandı`);
+        });
+
+        section.append(imageButton);
+    });
+}
 
 year.textContent = new Date().getFullYear();
-let isMouseDown = false;
-let currentMode = "paint";
-let gridSize = Number(slider.value);
-let gridVisible = true;
